657f06462ae0b3bb16f2d0d395453de0ff30bd68
Peter Klein <peter@uytr.org>
2012-11-20 00:10:37 -0500

    Import VS Project

---
+++ Controller/DrinkBotConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DrinkBotLib;
+using DrinkBotLib.Model;
+
+
+namespace DrinkBotConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialDrinkBot b = SerialDrinkBot.Local;
+            
+            using (var db = new DrinkBotEntities())
+            {
+                b.Dispense(db.Recipes.Single(r => r.Name == "Gin and Tonic"));
+            }
+
+            
+        }
+    }
+}
