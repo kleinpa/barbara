729c7b1f3ec4780eb44f6a3f4fc43d6833b38df5
Peter Klein <peter@uytr.org>
2015-05-06 08:45:37 -0400

    Add database proof of concept WIP

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,24 @@
+var models  = require('../models');
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   require('../lib/rpitest').toggle()
-  res.send('respond with a resource');
+  models.User.findAll({
+  }).then(function(users) {
+    res.render('index', {
+      title: 'Express',
+      users: users
+    });
+  });
+});
+
+router.post('/create', function(req, res) {
+  models.User.create({
+    username: req.param('username')
+  }).then(function() {
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
