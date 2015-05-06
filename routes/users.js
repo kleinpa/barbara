50199667d6ac9a015eb733ffc2d4580bbc6fa6cc
Peter Klein <peter@uytr.org>
2015-05-06 00:49:44 -0400

    Add proof of concept LED control

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  require('../lib/rpitest').toggle()
   res.send('respond with a resource');
 });
 
