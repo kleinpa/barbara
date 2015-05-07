06a78e2f4ff8af8da98da5252f5bb2a09b221b5f
Peter Klein <peter@uytr.org>
2015-05-07 00:04:37 -0400

    Add test page

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,28 @@
 (function () {
   'use strict';
   angular.module('main', ['ngRoute'])
+    .factory('socket', function ($rootScope) {
+      var socket = io.connect();
+      return {
+	on: function (eventName, callback) {
+	  socket.on(eventName, function () {
+            var args = arguments;
+            $rootScope.$apply(function () {
+              callback.apply(socket, args);
+            });
+	  });
+	},
+	emit: function (eventName, data, callback) {
+	  socket.emit(eventName, data, function () {
+            var args = arguments;
+            $rootScope.$apply(function () {
+              if (callback) {
+		callback.apply(socket, args);
+              }
+            });
+	  });
+	}};
+    })
     .controller('CtrlMain', function ($scope, $http, $interval) {
     })
     .config(['$routeProvider', '$locationProvider',
@@ -20,8 +42,48 @@
           when('/users/', {
             templateUrl: '/partials/users.html'
           }).
+          when('/test/', {
+            templateUrl: '/partials/test.html',
+            controller: 'CtrlTest'
+          }).
           otherwise({
             redirectTo: '/'
           });
-      }]);
+      }])
+    .controller('CtrlTest', function ($scope, $http, $window, socket) {
+      $scope.channels = [0, 1, 2, 3, 4];
+      var channel_states = {};
+      socket.on('init', function (data) {
+	$scope.name = data.name;
+	$scope.users = data.users;
+      });
+      $scope.set = function(channel, state, x){
+	if (channel_states[channel] != state) {
+	  socket.emit('test-set', {c: channel, s:state});
+	  channel_states[channel] = state;
+	}
+      };
+
+      var keyDown = function(e) {
+	var n = e.which - 49;
+	if(n >= 0 && n < 5){
+	  $scope.set(n, 1);
+	}
+      };
+      var keyUp = function(e) {
+	var n = e.which - 49;
+	if(n >= 0 && n < 5){
+	  $scope.set(n, 0);
+	}
+      };
+      angular.element($window).on('keydown', keyDown);
+      angular.element($window).on('keyup', keyUp);
+      $scope.$on('$destroy', function()
+		 {
+		   angular.element($window).off('keyup', keyUp);
+		   angular.element($window).off('keydown', keyDown);
+		 });
+
+      $scope.z = 4;
+    });
 }());
