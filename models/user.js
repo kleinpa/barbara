ef175fcd727df0898454561f6b833c06ecd08428
Peter Klein <peter@uytr.org>
2015-05-12 18:28:34 -0400

    Create user model

--- models/user.js
+++ models/user.js
@@ -1,10 +1,13 @@
-"use strict";
-
-var Sequelize = require('sequelize');
-
+'use strict';
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    username: {type: Sequelize.STRING},
+    name: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
   });
   return User;
 };
