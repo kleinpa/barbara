a6067a5b263df2ee2a4c273ef0d585f7962a3517
Peter Klein <peter@uytr.org>
2015-05-13 22:25:17 -0400

    Create Recipe model

--- models/recipe.js
+++ models/recipe.js
@@ -1,14 +1,12 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define('User', {
+  var Recipe = sequelize.define('Recipe', {
     name: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
-        models.Recipe.hasMany(models.RecipeIngredient, {as: 'Ingredients'})
-        User.hasMany(models.Log)
       }
     }
   });
-  return User;
+  return Recipe;
 };
